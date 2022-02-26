7b29d7308919a1ade4be2164accae4820b5894d2
magicHack3r <dnjsdlf61@naver.com>
2022-02-26 15:57:08 +0900

    2022.02.26

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Author; magicHack3r for my own ease.
+echo "      
+                  _    _  _         _   ____    
+  _ __  __ _ __ _(_)__| || |__ _ __| |_|__ /_ _ 
+ | '  \/ _\ / _\ | / _| __ / _\ / _| / /|_ \ '_|
+ |_|_|_\__,_\__, |_\__|_||_\__,_\__|_\_\___/_|  
+            |___/                               
+
+"
+
+echo "Enter setup number you want;"
+echo "[1] pwntools"
+echo "[2] checksec"
+echo "[3] sublime"
+echo "[4] exit"
+read -p "Number: " number
+
+if [ $number -eq 1 ]; then 
+    echo "[+] Setting up pwntools."
+    apt-get update
+    apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential -y
+    python3 -m pip install --upgrade pip
+    python3 -m pip install --upgrade pwntools
+
+elif [ $number -eq 2 ]; then 
+    echo "[+] Setting up checksec."
+    checksec setup
+    git clone https://github.com/slimm609/checksec.sh.git
+    cd checksec.sh
+    cp checksec /usr/local/bin
+
+elif [ $number -eq 3 ]; then
+    echo "[+] Setting up sublime."
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    sudo apt-get install apt-transport-https
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list 
+
+else
+    echo "quit"
+fi
+
